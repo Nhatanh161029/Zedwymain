2439446df0f4e3b422da4bbd3131c2a19be021e9
Nhatanh161029 <122199937+Nhatanh161029@users.noreply.github.com>
2024-01-09 21:19:19 +0700

    Update Zedwymain.lua

--- Zedwymain.lua
+++ Zedwymain.lua
@@ -1,105 +1,52 @@
+repeat wait() until game:IsLoaded()
+_G["Fumo Blox Fruit - HoHo Hub Kaitun V3"] = {
+    MAIN_UI_COLOR = Color3.fromRGB(255, 165, 0), -- Color3.fromRGB(170, 85, 255)
+    
+    BlackScreen = true, -- less lag
+    FastMode = true, -- true/false less lag
+    AutoRejoinGame = 1800, -- time in second, rejoin to bypass anticheat
+    GetFruitInWorld = true, -- true/false collect fruit
+    AutoTeam = "Pirate", -- Pirate/Marine
+    GetMelees = {"Superhuman", "Death Step","Sharkman Karate","Electric Claw","Dargon Talon", "Godhuman"}, -- remove if u dont wanna get what melee
+    Redeem_X2EXP_on = 10, -- to redeem all x2 exp code on what level
+    Character_Config = {
+        InvisFromKen = true, -- true/false anyone use ken cant see your name
+        NoStun = true, -- true/false make u dont get stuned when farming
+    },
+    ListFruitTake = {
+        "Ice-Ice","Light-Light","Spider-Spider",'Magma-Magma','Flame-Flame','Rumble-Rumble','Dark-Dark','Quake-Quake','Buddha-Buddha',
+        -- put fruit you want like above, recommend fruit can awake
+        -- fruit list: 'Sand-Sand','Smoke-Smoke','Ice-Ice','Kilo-Kilo','Diamond-Diamond','Light-Light','Shadow-Shadow','Portal-Portal','Spider-Spider','Gravity-Gravity','Spin-Spin','Revive-Revive','Bird-Bird: Phoenix','Rubber-Rubber','Spring-Spring','Blizzard-Blizzard','Dough-Dough','Bomb-Bomb','Venom-Venom','Bird-Bird: Falcon','Spirit-Spirit','Love-Love','Leopard-Leopard','Dragon-Dragon','Spike-Spike','Magma-Magma','Control-Control','Flame-Flame','Rumble-Rumble','Dark-Dark','Chop-Chop','Quake-Quake','Buddha-Buddha','Barrier-Barrier',"Rocket-Rocket","Sound-Sound","Pain-Pain","Mammoth-Mammoth","Kitsune-Kitsune","T-Rex-T-Rex"
+    },
+    BuyBetterFruit = true, --true/false if u alr have fruit but better fruit stock it will buy it
+    EatFruitWhenSea3 = true, --true/false we need to keep fruit before third sea
+    AwakeFruit = true, --true/false awake after a raid
+    AutoBuy = {
+        Shop_Items = true, --true/false auto buy all item sell with beli like Bisento,... we will buy when max level
+        All_Haki = true, --true/false ken,buso,geppo,soru we will buy this in first
+        Legendary_Swords = true, -- true/false buy all leg sword
+        Ectoplasm_Items = true, -- true/false buy all ecto items
+    },
+    AutoItems = {
+        ["Second Sea"] = {"Kabucha"},
+        ["Third Sea"] = {"Rainbow Haki"},
+        --remove rainbow haki or any thing if u dont want
+    },
+    Deep_Config = {
+        -- Delay go to Third Sea
+        ["Ectoplasm Farm"] = {
+            Enabled = true, --true/false enabled is needable if u want Soul Guitar
+            FarmUntil = 250, -- 250 = soul guitar price
+        },
+        ["Dark Beard Drop Farm"] = false, -- enabled this if u want 100% get Soul Guitar
+        ["Full 3 Leg Sword and True Triple Katana"] = false, -- enabled this if u want 100% get true triple katana
+    },
+    HopServerFarm = true, -- true/false why not?
+    StopRaidWhenGetEnoughFrag = 10000, --10k is enough so we will farm faster
+    PVP_FARM = true, -- kill player grind exp fast
+    ShowAccountInfo = true, -- an ui with your account information
+}
 
-repeat task.wait() until game.Players
-repeat task.wait() until game.Players.LocalPlayer
-repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
-repeat task.wait() until game.Players.LocalPlayer.PlayerGui:FindFirstChild("Main");
-_G.Team = "Pirate" -- Marine / Pirate
-_G.KAITUN_SCRIPT = true
-_G.LogsDes = {
-    ["Enabled"] = false, -- เปิดการใช้งาน
-    ["SendAlias"] = false, -- เปิดการส่ง Alias
-    ["SendDescription"] = false, -- เปิดการส่ง Des
-    ["DelaySend"] = 5 -- วินาที
-}
-_G.WebHook = {
-    ["Enabled"] = false, -- เปิดการใช้งาน
-    ["Url"] = "", -- ลิ้งค์เว็บฮุก
-    ["Delay"] = 60 -- วินาที
-}
-_G.MainSettings = {
-        ["EnabledHOP"] = false, -- เปิด HOP ( มันไม่มีอยู่ละใส่มาเท่ๆ )
-        ['FPSBOOST'] = true, -- ภาพกาก
-        ["FPSLOCKAMOUNT"] = 60, -- จำนวน FPS
-        ['WhiteScreen'] = true, -- จอขาว
-        ['CloseUI'] = true, -- ปิด Ui
-        ["NotifycationExPRemove"] = true, -- ลบ ExP ที่เด้งตอนฆ่ามอน
-        ['AFKCheck'] = 60, -- ถ้ายืนนิ่งเกินวิที่ตั้งมันจะรีเกม
-        ["LockFragments"] = 12000, -- ล็อคเงินม่วง
-        ["LockFruitsRaid"] = { -- ล็อคผลที่ไม่เอาไปลงดัน
-            [1] = "Kitsune-Kitsune",
-            [2] = "Dragon-Dragon"
-        }
-    }
-_G.Fruits_Settings = { -- ตั้งค่าผล
-    ['Main_Fruits'] = {'Magma-Magma'}, -- ผลหลัก ถ้ายังไม่ใช่ค่าที่ตั้งมันจะกินจนกว่าจะใช่หรือซื้อ
-    ['Select_Fruits'] = {"Flame-Flame", "Ice-Ice", "Quake-Quake", "Light-Light", "Dark-Dark", "Spider-Spider", "Rumble-Rumble", "Magma-Magma", "Buddha-Buddha"} -- กินหรือซื้อตอนไม่มีผล
-}
-_G.Quests_Settings = { -- ตั้งค่าเควสหลักๆ
-    ['Rainbow_Haki'] = false,
-    ["MusketeerHat"] = false,
-    ["PullLever"] = false,
-    ['DoughQuests_Mirror'] = {
-        ['Enabled'] = false,
-        ['UseFruits'] = false
-    }        
-}
-_G.Races_Settings = { -- ตั้งค่าเผ่า
-    ['Race'] = {
-        ['EnabledEvo'] = false,
-        ["v2"] = false,
-        ["v3"] = false,
-        ["Races_Lock"] = {
-            ["Races"] = { -- Select Races U want
-                ["Mink"] = false,
-                ["Human"] = false,
-                ["Fishman"] = false
-            },
-            ["RerollsWhenFragments"] = 20000 -- Random Races When Your Fragments is >= Settings
-        }
-    }
-}
-_G.Settings_Melee = { -- หมัดที่จะทำ
-    ['Superhuman'] = true,
-    ['DeathStep'] = true,
-    ['SharkmanKarate'] = true,
-    ['ElectricClaw'] = true,
-    ['DragonTalon'] = true,
-    ['Godhuman'] = true
-}
-_G.FarmMastery_Settings = {
-    ['Melee'] = true,
-    ['Sword'] = false,
-    ['DevilFruits'] = false,
-    ['Select_Swords'] = {
-        ["AutoSettings"] = false, -- ถ้าเปิดอันนี้มันจะเลือกดาบให้เองหรือฟาร์มทุกดาบนั่นเอง
-        ["ManualSettings"] = { -- ถ้าปรับ AutoSettings เป็น false มันจะฟาร์มดาบที่เลือกตรงนี้ ตัวอย่างข้างล่าง
-            "",
-            ""
-        }
-    }
-}
-_G.SwordSettings = { -- ดาบที่จะทำ
-    ['Saber'] = false,
-    ["Pole"] = false,
-    ['MidnightBlade'] = false,
-    ['Shisui'] = false,
-    ['Saddi'] = false,
-    ['Wando'] = false,
-    ['Yama'] = false,
-    ['Rengoku'] = false,
-    ['Canvander'] = false,
-    ['BuddySword'] = false,
-    ['TwinHooks'] = false,
-    ['HallowScryte'] = false,
-    ['TrueTripleKatana'] = false,
-    ['CursedDualKatana'] = false
-}
-_G.GunSettings = { -- ปืนที่จะทำ
-    ['Kabucha'] = false,
-    ['SerpentBow'] = false,
-    ['SoulGuitar'] = false
-}
-getgenv().Key = "MARU-9UTZV-5WBN1-HQBO-PYXUK-AKCU"
-getgenv().id = "1094789671730561054"
-getgenv().Script_Mode = "Kaitun_Script"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
+_G.BypassTeleport = false -- set it true if u wanna fast tp (idk if bug or not)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HohoV2/main/ScriptLoad.lua"))()
